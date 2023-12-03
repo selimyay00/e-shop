3d1fa4063ee30dafeb79b5e94bae78d8f4b6aa23
selim yay <selimxyay@gmail.com>
2023-12-03 13:42:59 +0300

    made navbar responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,9 @@ import './Header.css';
 function Header() {
     return (
         <div className='header'>
-            <h1>version-1 branch header</h1>
             <div className='header__section1'>
                 <StorefrontIcon className='store-icon' />
-                <h4>E-Shop</h4>
+                <h4 className='link-text'>E-Shop</h4>
             </div>
             <div className='header__section2'>
                 <input type="text" />
@@ -18,12 +17,12 @@ function Header() {
             </div>
             <div className='header__section3'>
                 <div className='links'>
-                    <h5>Hello Guest</h5>
-                    <h3 style={{marginTop: -1}}>Sign In</h3>
+                    <h5 className='link-text'>Hello Guest</h5>
+                    <h3 style={{marginTop: -1}} className='link-text'>Sign In</h3>
                 </div>
                 <div className='links'>
-                    <h5>Your</h5>
-                    <h3 style={{marginTop: -1}}>Shop</h3>
+                    <h5 className='link-text'>Your</h5>
+                    <h3 style={{marginTop: -1}} className='link-text'>Shop</h3>
                 </div>
                 <div className='shop-icon'>
                 <ShoppingCartIcon size="large" />
