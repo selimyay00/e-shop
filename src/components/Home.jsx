5cff239bd4f6277cdb3abf56ad133e3883fc45a9
selim yay <selimxyay@gmail.com>
2023-12-04 10:35:04 +0300

    started to add props to each product

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,65 @@
 import './home.css'
 import eCommerceImage from '../images/e-commerce.jpg';
 import Product from './Product';
+import bag from '../images/bag.jpg';
+import tv from '../images/tv.jpg';
+import iphone from '../images/iphone.jpg';
+import camera from '../images/camera.jpg';
+import watch from '../images/watch.jpg';
+import airpods from '../images/airpods.jpg';
 
 function Home() {
     return (
         <div>            
                 <img src={eCommerceImage} alt="e-commerce-image" className='home-image' />
                 <div className='first-row'>
-                    <Product />
-                    <Product />
+                    <Product 
+                        id='1'
+                        title='Bennett Mystic 15.6 inch Laptop
+                        for Men and Women'
+                        price={35}
+                        rating={3}
+                        image={bag}
+                    />
+                    <Product 
+                        id='2'
+                        title='TV Unveiled: The Nexus Chronicles - Unraveling Realms'
+                        price={100}
+                        rating={4}
+                        image={tv}
+                    />
                 </div>
                 <div className='second-row'>
-                    <Product />
-                    <Product />
-                    <Product />
+                    <Product 
+                        id='3'
+                        title='iPhone 14'
+                        price={100}
+                        rating={4}
+                        image={iphone}
+                    />
+                    <Product 
+                        id='4'
+                        title='CaptureSphere: Lenscrafting moments'
+                        price={100}
+                        rating={4}
+                        image={camera}
+                    />
+                    <Product 
+                        id='5'
+                        title='TimeCraft Elegance: Wrist Symphony'
+                        price={100}
+                        rating={4}
+                        image={watch}
+                    />
                 </div>
                 <div className='third-row'>
-                    <Product />
+                    <Product 
+                        id='6'
+                        title='TimeCraft Elegance: Wrist Symphony'
+                        price={100}
+                        rating={4}
+                        image={airpods}
+                    />
                 </div>
         </div>
     )
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,19 +1,19 @@
 import './product.css'
 import { FaStar } from 'react-icons/fa';
-import bag from '../images/bag.jpg';
 
-function Product() {
+function Product(props) {
     return (
         <div className='product'>
             <div className='product__info'>
                 <p>
-                    Bennett Mystic 15.6 inch Laptop
-                    for Men and Women
+                    {props.title}
                 </p>
-                <p className='product__price'><i>$30</i></p>
+                <p className='product__price'>
+                    {props.price}
+                </p>
                 <FaStar className='product__rating' />
             </div>
-            <img src={bag} alt="bag" className='product__images' />
+            <img src={props.image} alt="images" className='product__images' />
             <button>Add to basket</button>
         </div>
     )
